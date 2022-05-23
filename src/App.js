576b6e9ab3f5aa8dcf8ec0346e07b8af08e44334
git@bl-mbp-mysql.southindia.cloudapp.azure.com <anand.s@billionlives.in>
2022-05-23 23:48:04 +0530

    New module created

--- src/App.js
+++ src/App.js
@@ -1,27 +1,15 @@
 import React  from "react";
+import Test from "./Components/testproject";
 
 
 
-var a=10;
 export default class testclass extends React.Component{
 
-update=()=>{
-a=a+10;
-alert(a);
-<div>aaa</div>
-  
-}
 
 render(){
-return(
-<div>
-
-<button onClick={this.update}>Click to Add </button>
-
-</div>
-
+  return(
+<Test/>
 )
-
 }
 
 }
---
+++ src/Components/testproject.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+
+var a=10;
+export default class test extends React.Component{
+
+
+    update=()=>{
+        a=a+10;
+        alert(a);
+             
+        }
+        
+        render(){
+        return(
+        <div>
+        
+        <button onClick={this.update}>Click to Add Value </button>
+        
+        </div>
+        
+        )
+        
+        }
+}
