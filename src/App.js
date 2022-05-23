f3a7303094483e5235a78ac691f2d5b61c735106
git@bl-mbp-mysql.southindia.cloudapp.azure.com <anand.s@billionlives.in>
2022-05-23 23:29:08 +0530

    new project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React  from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+var a=10;
+export default class testclass extends React.Component{
+
+update=()=>{
+a=a+10;
+alert(a);
+<div>aaa</div>
+  
 }
 
-export default App;
+render(){
+return(
+<div>
+
+<button onClick={this.update}>Click to Add </button>
+
+</div>
+
+)
+
+}
+
+}
